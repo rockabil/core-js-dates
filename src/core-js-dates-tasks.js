8601d727caf17da6a57f48a6bb2aafc1ea46ddb5
rockabil <rockabil81@gmail.com>
2026-01-03 20:30:10 +0300

    feat: add implementation of getNetFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -83,8 +83,11 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const next = new Date(date);
+  const add = (5 - next.getDay() + 7) % 7 || 7;
+  next.setDate(next.getDate() + add);
+  return next;
 }
 
 /**
